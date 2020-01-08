8d3c5653f00efbe74cbe8349cbda49b047dd6007
Egor <egor.eskin@yandex.ru>
2020-01-08 17:07:31 +0500

    make it better

---
+++ WebApplication1/Controllers/CallBackController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using VkNet.Abstractions;
+using VkNet.Model;
+using VkNet.Utils;
+using VkNet.Model.RequestParams;
+using WebApplication1.Controllers;
+using System;
+
+namespace VkBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallbackController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        private readonly IVkApi _vkApi;
+
+     
+        /// <summary>
+        /// Конфигурация приложения
+        /// </summary>
+
+
+        public CallbackController(IVkApi vkApi, IConfiguration configuration)
+        {
+            _vkApi = vkApi;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Callback([FromBody] Updates updates)
+        {
+            // Тип события
+            switch (updates.Type)
+            {
+                // Ключ-подтверждение
+                case "confirmation":
+                    {
+                        return Ok(_configuration["Config:Confirmation"]);
+                    }
+
+                // Новое сообщение
+                case "message_new":
+                    {
+                        // Десериализация
+                        var msg = Message.FromJson(new VkResponse(updates.Object));
+                        _vkApi.Messages.Send(new MessagesSendParams
+                        {
+                            RandomId = new DateTime().Millisecond,
+                            PeerId = msg.PeerId.Value,
+                            Message = msg.Text
+                        });
+                        break;
+                    }
+            }
+
+            return Ok("ok");
+        }
+    }
+
+}
